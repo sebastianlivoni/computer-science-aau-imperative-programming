8458aca84e43d58464aaac0b8263c1d5009bc6dd
Sebastian Livoni <seb@livoni.me>
2021-10-27 23:04:02 +0200

    made exercise 11 on page 452 better with comments

--- exercises/exercises-in-book/452.11_v2.c
+++ exercises/exercises-in-book/452.11_v2.c
@@ -12,58 +12,70 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void merge_arrays(double *merge_array, int *new_size, const double *arr1, int size_arr1, const double *arr2, int size_arr2);
+double* merge_arrays(const double *arr1, const int size_arr1, const double *arr2, const int size_arr2, int *new_size);
+void print_array(double* array, int size);
 
 int main(void) {
-  double sorted_arr1[] = {-10.5, -1.8, 3.5, 4.0, 6.3, 7.2, 7.3, 8.5, 8.6, 8.7, 8.8, 10.0, 10.10, 20.0, 28.0, 42.0, 43.0};
-  double sorted_arr2[] = {-50.0, -10.5, -1.8, 3.1, 3.6, 4.0, 6.3, 7.1, 7.2, 8.8, 9.9, 10.10, 20.0, 30.0, 40.0, 42.0, 43.0};
+  double *merged_arr,
+         sorted_arr1[] = {-10.5, -1.8, 3.5, 4.0, 6.3, 7.2, 7.3, 8.5, 8.6, 8.7, 8.8, 10.0, 10.10, 20.0, 28.0, 42.0, 43.0},
+         sorted_arr2[] = {-50.0, -10.5, -1.8, 3.1, 3.6, 4.0, 6.3, 7.1, 7.2, 8.8, 9.9, 10.10, 20.0, 30.0, 40.0, 42.0, 43.0};
 
   /* Get size of array 1 & array 2 & the the sum of these */
-  int size_of_arr1 = sizeof(sorted_arr1) / sizeof(double),
-      size_of_arr2 = sizeof(sorted_arr2) / sizeof(double),
-      max_arr_width = size_of_arr1 + size_of_arr2;
-
-  double *merged_arr = malloc(max_arr_width * sizeof(double));
-  int merged_arr_size;
+  int merged_arr_size,
+      size_of_arr1 = sizeof(sorted_arr1) / sizeof(double),
+      size_of_arr2 = sizeof(sorted_arr2) / sizeof(double);
 
   /* Merge the two array */
-  merge_arrays(merged_arr, &merged_arr_size, sorted_arr1, size_of_arr1, sorted_arr2, size_of_arr2);
+  merged_arr = merge_arrays(sorted_arr1, size_of_arr1, sorted_arr2, size_of_arr2, &merged_arr_size);
 
-  for (int i = 0; i < merged_arr_size; i++) {
-    printf("%.2f ", merged_arr[i]);
-  }
+  /* Print our array */
+  print_array(merged_arr, merged_arr_size);
 
-  printf("\n");
-
+  /* Clean up and other stuff */
   free(merged_arr);
+  return EXIT_SUCCESS;
 }
 
-void merge_arrays(double *merge_array, int *new_size, const double *arr1, const int size_arr1, const double *arr2, const int size_arr2) {
+double* merge_arrays(const double* arr1, const int size_arr1, const double* arr2, const int size_arr2, int* new_size) {
+  /* Allocating memory for the new merged array */
+  double* merged_arr = malloc((size_arr1 + size_arr2)* sizeof(double));
 
   int i = 0,
       count_arr1 = 0,
       count_arr2 = 0,
-      sum_of_arrays = size_arr1 + size_arr2;
+      size_of_arrays = size_arr1 + size_arr2;
 
-  while (i < sum_of_arrays) {
+  while (i < size_of_arrays) {
+    /* When both arrays still have elements */
     if (count_arr1 < size_arr1 && count_arr2 < size_arr2) {
       if (arr1[count_arr1] < arr2[count_arr2]) {
-        merge_array[i++] = arr1[count_arr1++];
+        merged_arr[i++] = arr1[count_arr1++];
       } else if (arr2[count_arr2] < arr1[count_arr1]) {
-        merge_array[i++] = arr2[count_arr2++];
+        merged_arr[i++] = arr2[count_arr2++];
       } else {
-        /* Either arr1 or arr2 - doesn't matter */
-        merge_array[i++] = arr1[count_arr1++];
-        count_arr2++;
-        sum_of_arrays--;
+        /* Arr1 & arr2 is equal */
+        merged_arr[i++] = arr1[count_arr1++];
+        count_arr2++; /* We want to skip this value and count to next one */
+        size_of_arrays--; /* Because both values are the same, the size of the array is going to be 1 value smaller */
       }
     } else if (count_arr1 < size_arr1) {
-      merge_array[i++] = arr1[count_arr1++];
+      /* Only arr1 left - adding the values left of array 1 to the merged array */
+      merged_arr[i++] = arr1[count_arr1++];
     } else if (count_arr2 < size_arr2) {
-      merge_array[i++] = arr2[count_arr2++];
+      /* Only arr2 left - adding the values left of array 2 to the merged array */
+      merged_arr[i++] = arr2[count_arr2++];
     }
   }
 
   *new_size = i;
-  merge_array = realloc(merge_array, *new_size * sizeof(double));
+  /* Reallocate memory */
+  return (double*) realloc(merged_arr, *new_size * sizeof(double));
+}
+
+void print_array(double* array, int size) {
+  int i;
+  for (i = 0; i < size; i++) {
+    printf("%.2f ", array[i]);
+  }
+  printf("\n");
 }
