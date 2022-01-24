758c2710b34e2715c9ef6947b6da3c7545ce8085
Sebastian Livoni <safe.tea7364@livoni.me>
2022-01-24 17:00:04 +0100

    removed comment

--- other/mergesort.c
+++ other/mergesort.c
@@ -1,14 +1,3 @@
-/*
- * Title: 
- * Description: 
- * Assignment: 
- * Link to assignment: 
- * 
- * Programmer: Sebastian Livoni Larsen
- * Date completed: November 5, 2021
- * Instructor: Kurt Nørmark * Class: AAL E21
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
